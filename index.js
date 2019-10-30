1982bbcb13a8743bfbfe23dcbf64ea7677cd36da
fillika <49833052+fillika@users.noreply.github.com>
2019-10-30 08:04:49 +0300

    Решил работать в единственном файле JS, не понял в чем ошибка

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import producdb from "./js/Module";
+const producdb = (dbname, table) => {
+  const db = new Dexie(dbname);
+  db.version(1).stores(table);
+  db.open();
+  /*
+	db.version(1).stores({
+			friends: `name,age`
+		});
+		*/
+  return db;
+};
 
-producdb();
+let db = producdb("Productdb", {
+  products: `++id, name, seller, price`
+});
+
+//input tags
+const userId = document.getElementById('userid')
+const proname = document.getElementById('proname')
+const seller = document.getElementById('seller')
+const price = document.getElementById('price')
