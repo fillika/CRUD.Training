265b36999b347afed77ebcd6b1d7155631c3dc4e
fillika <49833052+fillika@users.noreply.github.com>
2019-10-30 08:24:05 +0300

    Добавил функцию проверки заполненных полей, а так же добавление в базу и получение кол-ва товаров в базе

--- index.js
+++ index.js
@@ -15,7 +15,61 @@ let db = producdb("Productdb", {
 });
 
 //input tags
-const userId = document.getElementById('userid')
-const proname = document.getElementById('proname')
-const seller = document.getElementById('seller')
-const price = document.getElementById('price')
+const userId = document.getElementById("userid");
+const proname = document.getElementById("proname");
+const seller = document.getElementById("seller");
+const price = document.getElementById("price");
+
+// buttons
+
+const btnCreate = document.getElementById("btn-create");
+const btnRead = document.getElementById("btn-read");
+const btnUpdate = document.getElementById("btn-update");
+const btnDelete = document.getElementById("btn-delete");
+
+btnCreate.addEventListener("click", function() {
+  let flag = bulkCreate(db.products, {
+    name: proname.value,
+    seller: seller.value,
+    price: price.value
+  });
+
+  proname.value = seller.value = price.value = "";
+  getData();
+});
+
+//insert function
+const bulkCreate = (dbtable, data) => {
+  let flag = empty(data);
+
+  if (flag) {
+    dbtable.bulkAdd([data]);
+    console.log("Data insert successfully...");
+  } else {
+    console.log("Plesase provide data");
+  }
+  return flag;
+};
+
+//check textbox validation
+const empty = obj => {
+  let flag = false;
+
+  for (const value in obj) {
+    if (obj[value] != "" && obj.hasOwnProperty(value)) {
+      flag = true;
+    } else {
+      flag = false;
+    }
+  }
+  return flag;
+};
+
+const getData = () => {
+  let index = 0;
+  let obj = {};
+
+  db.products.count(count => {
+    console.log(count);
+  });
+};
